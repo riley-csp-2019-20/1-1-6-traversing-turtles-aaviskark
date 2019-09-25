cd86065199506601e50301772bc7cd9704d47ac2
aaviskark <55500875+aaviskark@users.noreply.github.com>
2019-09-25 11:19:01 -0400

    completed upload

--- a116_traversing_turtles_AaviskarKhatiwada.py
+++ a116_traversing_turtles_AaviskarKhatiwada.py
@@ -11,20 +11,23 @@ turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
+  new_color= turtle_colors.pop()
+  t.color(new_color)
+  my_turtles.append(t) 
 
 # starts the pen at (0,0)
 startx = 0
 starty = 0
 
 # calls the turtles to move
+count=1
 for t in my_turtles:
 	t.penup()
 	t.goto(startx, starty)
 	t.pendown()
-	t.right(45)     
+	t.right(45*count)     
 	t.forward(50)
-	t.color
+	count += 1
 
 
 	
